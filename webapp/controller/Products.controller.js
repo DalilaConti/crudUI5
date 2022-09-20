aca54ae71d3f12650813715ce3d9ecc84fc71016
dalila Conti <dalila.contigoyeneche@gmail.com>
2022-09-20 18:42:56 +0000

    added update function

--- webapp/controller/Products.controller.js
+++ webapp/controller/Products.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Controller, _MessageToast) {
         "use strict";
 
         return Controller.extend("globant.productos.controller.Products", {
@@ -13,28 +13,50 @@ sap.ui.define([
             onInit: function () {
 
             },
-            onSave: function (){
-              const oModel = this.getView().getModel();
-              const data = {
-                  firstName :"fname",
-                  lastName : "lname",
-                  companyName: "cname",
-                  city: "city"
+            onCreate: function (){
+                const ID = this.getView().byId("pID").getValue(); 
+                const name = this.getView().byId("pname").getValue();
+                const data = {
+                  ID :ID,
+                  Name: name,
+                 
                 };
-              oModel.create("/Customers", data, null,{
-                  succes:function(oData){
-                      sap.m.MessageToast.show('Customer added successfully');
+                
+             const oModel = this.getView().getModel();
+              oModel.create("/Products", data, {
+                  succes:function(_oData){
+                      sap.m.MessageToast.show('Product added successfully');
                   }.bind(this),
                   error: function(){
-                    sap.m.MessageToast.show('Failed to add customer');
+                    sap.m.MessageToast.show('Failed to add Product');
             }.bind(this)
-          });
+          
+              });
+              },
+
+              onUpdate: function(){
+                   const list = this.getView().byId("list");
+                  const selItem = list.getSelectedItem();
+                   const title = selItem.getTitle();
+                    const description = selItem.getDescription();
+                   const Name = this.getView().byId("pname").getValue();
+               
+                    const payload = {
+                       ID: parseInt(title),
+                        Name: Name
+                   };
+    
+                    const path = "/Products(" + title + ")";
+                    const odataModel = this.getView().getModel();
+                    odataModel.update(path,payload,{
+                        succes: function (data,response) {
+                            sap.m.MessageToast.show('Product updated successfully');
+                        }.bind(this),
+                        error: function(){
+                          sap.m.MessageToast.show('Failed to update Product');
+                  }.bind(this)
+                
+                });
             }
-    });
         });
-    
-              
-        
-   
-    
-        
+    });
