1687b1d0471d2a255f8bf508a8fadcf41d04164e
dalila Conti <dalila.contigoyeneche@gmail.com>
2022-09-21 16:56:10 +0000

    added changes on controller

--- webapp/controller/Products.controller.js
+++ webapp/controller/Products.controller.js
@@ -24,7 +24,7 @@ sap.ui.define([
                 
              const oModel = this.getView().getModel();
               oModel.create("/Products", data, {
-                  succes:function(_oData){
+                  success:function(_oData){
                       sap.m.MessageToast.show('Product added successfully');
                   }.bind(this),
                   error: function(){
@@ -33,24 +33,40 @@ sap.ui.define([
           
               });
               },
-              onUpdate: function(){
-                const ID = this.getView().byId("pID").getValue(); 
+              onUpdate: function () {
+                // Primero obtenemos el 'path' del producto que queremos modificar
+                // para eso, de nuestra lista tomamos el item seleccionado y mediante los
+                // métodos que nos da SAPUI5, obtendremos el path. sContext valdrá algo como "Products(123)"
+                const sContext = this.byId("list").getSelectedItem().getBindingContextPath();
                 const name = this.getView().byId("pname").getValue();
-              const oData = {
-                ID :ID,
-                Name: name,
-              }
-              const oModel = this.getView().getModel();
-              oModel.update("/Products(10)", oData,{
-                     succes: function (_odata) {
-                         sap.m.MessageToast.show('Product updated successfully');
-                     }.bind(this),
-                    
-                     error: function(){
-                       sap.m.MessageToast.show('Failed to update Product');
-               }.bind(this)
-                
+                const ID = this.getView().byId("pID").getValue();
+                // Cuando vamos a modificar un elemento, no es necesario enviar todos los campos, solo
+                // aquellos que queremos modificar para no sobrecargar el programa. En este caso, no 
+                // sería lo ideal permitir la modificación del ID, así que sólo enviamos el nuevo nombre. 
+                const oData = {
+                    Name: name,
+                    ID: ID
+                }
+                const oModel = this.getView().getModel();
+                // Cuando vamos a enviar nuestro put a backend, en lugar de usar un path estático
+                // usamos uno dinámico que apunte al producto que queremos modificar. 
+                oModel.update(sContext, oData, {
+                    success: function (_odata) {
+                        sap.m.MessageToast.show('Product updated successfully');
+                    }.bind(this),
+                    error: function (oError) {
+                        console.log("Error: ", oError);
+                        sap.m.MessageToast.show('Failed to update Product');
+                    }.bind(this)
+              
+                // NOTA: Fijate que sería lo ideal que agregues más validaciones. Te recomiendo las 
+                // siguientes: 
+                // 1) Validar que el usuario no modifique campos clave como el id
+                // 2) Validar que el usuario no envie un string vacio como nombre
+            
                 });
             }
         });
     });
+     
+   
